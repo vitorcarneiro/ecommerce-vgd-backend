aacc2d55ea81f688cc6e199a8290a30d4db7bc48
vitorcarneiro <vitorcarneiro.sdo@gmail.com>
2022-02-09 10:27:35 -0300

    chore: adjust code for deploy on heroku

---
+++ src/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+//import router from './routes/index.js';
+
+const server = express();
+server.use(express.json(), cors());
+
+//server.use(router);
+
+server.listen(process.env.PORT, () => {
+    console.log("Server running on port " + process.env.PORT);
+});
