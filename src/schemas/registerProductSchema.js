700d565a4c818cf12e4f03928288f5eeef269721
vitorcarneiro <vitorcarneiro.sdo@gmail.com>
2022-02-11 09:04:16 -0300

    fix: img added to schema

--- src/schemas/registerProductSchema.js
+++ src/schemas/registerProductSchema.js
@@ -2,6 +2,7 @@ import joi from "joi";
 
 const registerProductSchema = joi.object({
   name: joi.string().required(),
+  img: joi.string().pattern(new RegExp('(https?:\/\/.*\.(?:jpg|jpeg|png|gif|JPG|JPEG|PNG|GIF))')).required(),
   price: joi.number().positive().required(),
   inStock: joi.number().required(),
   isHighLight: joi.boolean()
