b83d802afa314c31bc67f2ccf83cbed3721b304a
vitorcarneiro <vitorcarneiro.sdo@gmail.com>
2022-02-12 12:20:45 -0300

    fix: unecessay lines

--- src/controllers/getProductsController.js
+++ src/controllers/getProductsController.js
@@ -3,7 +3,6 @@ import db from "../dataBase.js";
 export async function getProducts(req, res) {
   try {
     const products = await db.collection("products").find().toArray()
-  
     res.send(products);
     
   } catch (error) {
