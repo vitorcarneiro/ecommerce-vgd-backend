94777436b3012183e7f3e7c329864e9ffec88f19
pedro <pedrohperoni@gmail.com>
2022-02-12 21:34:37 -0300

    token

--- src/routers/postUserCart.js
+++ src/routers/postUserCart.js
@@ -3,7 +3,7 @@ import { postCart } from "../controllers/postCartController.js";
 import { tokenValidationMiddleware } from "../middlewares/tokenValidationMiddleware.js";
 
 const postUserCart = Router();
-postUserCart.use(tokenValidationMiddleware);
+// postUserCart.use(tokenValidationMiddleware);
 postUserCart.post("/cart", postCart);
 
 export default postUserCart;
