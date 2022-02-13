d63c21176b8e1c8efd1d407aa3835ecc5c9f8c56
pedro <pedrohperoni@gmail.com>
2022-02-12 21:33:28 -0300

    token

--- src/middlewares/tokenValidationMiddleware.js
+++ src/middlewares/tokenValidationMiddleware.js
@@ -4,12 +4,12 @@ export async function tokenValidationMiddleware(req, res, next) {
   const { authorization } = req.headers;
   const token = authorization?.replace("Bearer ", "");
 
-  if (!token) return res.sendStatus(200);
+  if (!token) return res.sendStatus(401);
 
   const session = await db.collection("sessions").findOne({ token });
 
   if (!session) {
-    return res.sendStatus(200);
+    return res.sendStatus(402);
   }
 
   //   const user = await db.collection("users").findOne({ _id: session.userId });
