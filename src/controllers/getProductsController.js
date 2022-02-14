f69fbb04e9f232b31408eeb7161496dcd276260b
vitorcarneiro <vitorcarneiro.sdo@gmail.com>
2022-02-14 10:25:49 -0300

    first commit brach

--- src/controllers/getProductsController.js
+++ src/controllers/getProductsController.js
@@ -2,6 +2,7 @@ import db from "../dataBase.js";
 
 export async function getProducts(req, res) {
   try {
+    console.log("ok");
     const products = await db.collection("products").find().toArray()
     res.send(products);
     
